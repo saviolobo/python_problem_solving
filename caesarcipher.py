d06095e174c4deb8b5486d3b708fee4e465914e8
Savio Lobo <saviolobo14@gmail.com>
2021-07-13 01:36:49 +0530

    Add files via upload

---
+++ caesarcipher.py
@@ -0,0 +1,33 @@
+#Caesar Cipher
+#Enter the message to generate cipher text
+s=input("Enter the secret message: ")
+
+#removing the spaces if present form the message
+w=s.replace(" ", "")
+
+#Check for only alphabets to be present
+bool=w.isalpha()
+
+if bool:
+    #enter the number to shift the letter of the message
+    shift=int(input("Enter the shift: "))
+    #initializing empty list
+    l=[]
+    n=w.lower()
+    
+    for i in w:
+        x=ord(i)-97
+        e=(x+shift)%26
+        y=e+97
+        c=chr(y)
+        print(c,end="")
+else:
+    print("Please enter only letters")
+
+    
+
+
+
+
+
+
