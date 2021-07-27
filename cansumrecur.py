3be4a6989a2a25c8c60cb123eb659be2b5918043
Savio Lobo <saviolobo14@gmail.com>
2021-07-27 20:02:38 +0530

    Add files via upload

---
+++ cansumrecur.py
@@ -0,0 +1,31 @@
+#given sum and array of elements,return true if the elements of array on adding give the sum or return false
+#elements in the array can be used as many times as needed to form sum
+#using recursion as brute force
+
+import array as arr
+a=arr.array('i',[])
+
+def cansum(tsum,a):
+    if (tsum==0):
+        return True
+    if (tsum<0):
+        return False
+    for num in a:
+        if (num>0):
+            rem=tsum-num
+            if cansum(rem,a)==True:
+                return True
+        else:
+            return "Numbers in array are either 0 or negative"
+    return False
+
+#in order to take input as target sum and array of numbers        
+tsum=int(input("Enter the target sum: "))
+n=int(input("Enter the number of elemets for the array: "))
+for i in range(0,n):
+    x=int(input("Enter the number: "))
+    a.append(x)
+
+result=cansum(tsum,a)
+
+print("Targets sum achieved?",result)
