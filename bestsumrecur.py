41e8e70207ad4949ac294740d1be59efa112a3e2
Savio Lobo <saviolobo14@gmail.com>
2021-07-28 18:12:15 +0530

    Add files via upload

---
+++ bestsumrecur.py
@@ -0,0 +1,36 @@
+#given sum and array of elements return an array having shortest combination that adds up to target sum
+#if tie return single one
+#using recursion as brute force
+
+import array as arr
+a=arr.array('i',[])
+comb=arr.array('i',[])
+
+def howsum(tsum,a):
+    if (tsum==0):
+        return []
+    if (tsum<0):
+        return None
+    best=None
+    for num in a:
+            rem=tsum-num
+            comb=howsum(rem,a)
+            if (comb!=None):   
+                comb.append(num)
+                if (best==None or len(comb)<len(best)):
+                    best=list(comb)
+                
+    return best
+              
+            
+#in order to take input as target sum and array of numbers        
+tsum=int(input("Enter the target sum: "))
+n=int(input("Enter the number of elemets for the array: "))
+for i in range(0,n):
+    x=int(input("Enter the number: "))
+    a.append(x)
+
+result=howsum(tsum,a)
+
+print("The combination that gives target sum: ",result)
+
